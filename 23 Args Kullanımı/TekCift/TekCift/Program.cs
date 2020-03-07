49135abf186890e817b35026aa1bda0af9dfc47f
Sefa Kozan <sefakozan05@gmail.com>
2020-03-07 23:25:56 +0300

    Argümanlar ile alakalı kısım eklendi.

---
+++ 23 Args Kullanımı/TekCift/TekCift/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TekCift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sayiStr = "";
+
+            Console.WriteLine("girilen arguman sayısı:{0}",args.Length);
+
+            if (args.Length > 0)
+            {
+                sayiStr = args[0];
+            }
+            else 
+            {
+                Console.Write("Konsola bir sayı giriniz : ");
+                sayiStr = Console.ReadLine();
+
+            }
+
+
+           
+            int sayiInt = Convert.ToInt32(sayiStr);
+
+            if (sayiInt % 2 == 0)
+            {
+                Console.WriteLine($"Girdiğiniz sayı ({sayiInt}) çiftdir");
+            }
+            else 
+            {
+                Console.WriteLine($"Girdiğiniz sayı ({sayiInt}) tekdir");
+            }
+
+            Console.WriteLine("çıkmak için herhangi bir tuşa basınız!.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 24 Carp Args/CarpArgs/CarpArgs/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarpArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sonuc;
+
+            if (args.Length == 3)
+            {
+                
+                Console.WriteLine("Girdiğiniz Argümanlar Sırasıyla : {0} {1} {2}",args[0], args[1], args[2]);
+
+
+                int sayi1 = Convert.ToInt32(args[0]);
+                int sayi2 = Convert.ToInt32(args[1]);
+                int sayi3 = Convert.ToInt32(args[2]);
+
+                sonuc = sayi1 * sayi2 * sayi3;
+                Console.WriteLine("Girdiğiniz Argümanların Çarpımı : {0}",sonuc);
+
+            }
+
+            if (args.Length > 3)
+            {
+                Console.WriteLine("Girdiğiniz Argümanlar Üçten Fazladır");
+                Console.WriteLine("Argümanları Yanlış Girdiğiniz için Program Sonlandı!");
+                Console.ReadKey();
+            }
+
+            if (args.Length < 3)
+            {
+                Console.WriteLine("Girdiğiniz Argümanlar Üçten Azdır");
+                Console.WriteLine("Argümanları Yanlış Girdiğiniz için Program Sonlandı!");
+                Console.ReadKey();
+            }
+
+        }
+    }
+}

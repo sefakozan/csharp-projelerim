5d0ed2a90c1fb9f487a4c9bbb9974ff12258cad0
Sefa Kozan <sefakozan05@gmail.com>
2020-03-25 17:22:41 +0300

    TempVariable projesi eklendi.

---
+++ 32 Variable Swap/VariableSwap/VariableSwap/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Konsola Birinci Sayıyı Giriniz : ");
+            string birincisayiStr = Console.ReadLine();
+            int birinciSayı = Convert.ToInt32(birincisayiStr);
+
+            Console.Write("Konsola İkinci Sayıyı Giriniz : ");
+            string ikincisayiStr = Console.ReadLine();
+            int ikinciSayı = Convert.ToInt32(ikincisayiStr);
+
+            Console.WriteLine("Birinci sayı : {0}  İkinci sayı : {1}",birinciSayı,ikinciSayı);
+
+
+
+            //
+            //tempVariable = Tempoaray Variable(Geçici Değişken)
+
+            int tempVariable = birinciSayı;
+            birinciSayı = ikinciSayı;
+            ikinciSayı = tempVariable;
+
+
+
+            Console.WriteLine("İki sayı değiştrildi.");
+            Console.WriteLine("Birinci sayı : {0}  İkinci sayı : {1}",birinciSayı,ikinciSayı);
+
+            Console.ReadKey();
+
+            
+        }
+    }
+}

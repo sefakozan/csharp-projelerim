4855f278eae94025be467e681c776953a0207914
Sefa Kozan <sefakozan05@gmail.com>
2020-03-25 17:39:01 +0300

    Variable swap without temp var projesi eklendi

---
+++ 33 Variable Swap Without Temp Var/VariableSwapWithoutTempVar/VariableSwapWithoutTempVar/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariableSwap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Konsola Birinci Sayıyı Giriniz : ");
+            string birincisayiStr = Console.ReadLine();
+            int birinciSayı = Convert.ToInt32(birincisayiStr);
+
+            Console.Write("Konsola İkinci Sayıyı Giriniz : ");
+            string ikincisayiStr = Console.ReadLine();
+            int ikinciSayı = Convert.ToInt32(ikincisayiStr);
+
+            Console.WriteLine("Birinci sayı : {0}  İkinci sayı : {1}", birinciSayı, ikinciSayı);
+
+
+
+            //
+
+
+            birinciSayı = birinciSayı + ikinciSayı;
+            ikinciSayı = birinciSayı - ikinciSayı;
+            birinciSayı = birinciSayı - ikinciSayı;
+
+
+
+            Console.WriteLine("İki sayı değiştrildi.");
+            Console.WriteLine("Birinci sayı : {0}  İkinci sayı : {1}", birinciSayı, ikinciSayı);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}

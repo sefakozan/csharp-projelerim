aa7348866c10a6ef8fab39405699a8adf441f24e
Sefa Kozan <sefakozan05@gmail.com>
2020-03-25 17:42:19 +0300

    Güncelleme yapıldı

--- 33 Variable Swap Without Temp Var/VariableSwapWithoutTempVar/VariableSwapWithoutTempVar/Program.cs
+++ 33 Variable Swap Without Temp Var/VariableSwapWithoutTempVar/VariableSwapWithoutTempVar/Program.cs
@@ -20,23 +20,23 @@ namespace VariableSwap
 
             Console.WriteLine("Birinci sayı : {0}  İkinci sayı : {1}", birinciSayı, ikinciSayı);
 
+            //Variable Swap Without =  Bu iş Mülaktlarda Hep sorulur.
+            //Geçiçi bir değişken kullanmadan yani temp variable kullanmadan iki değişkenin değerlerinin değiştirilmesi.
+            //bu işlem toplama çıkarma ile ya da çarpma bölme ile ya da bitwise kullanarak bu işlemler  yapılır.
 
-
-            //
-
+            //a=a+b;//a=30 (10+20)    
+            //b = a - b;//b=10 (30-20)    
+            //a = a - b;//a=20 (30-10)
+            //https://www.javatpoint.com/c-program-to-swap-two-numbers-without-using-third-variable
 
             birinciSayı = birinciSayı + ikinciSayı;
             ikinciSayı = birinciSayı - ikinciSayı;
             birinciSayı = birinciSayı - ikinciSayı;
 
-
-
             Console.WriteLine("İki sayı değiştrildi.");
             Console.WriteLine("Birinci sayı : {0}  İkinci sayı : {1}", birinciSayı, ikinciSayı);
 
             Console.ReadKey();
-
-
         }
     }
 }

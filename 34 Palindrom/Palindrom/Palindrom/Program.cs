a01d4a994ca063053f2d952ecc2ac761b2855694
Sefa Kozan <sefakozan05@gmail.com>
2020-03-28 13:31:07 +0300

    Polindrom dosya hali eklendi.

--- 34 Palindrom/Palindrom/Palindrom/Program.cs
+++ 34 Palindrom/Palindrom/Palindrom/Program.cs
@@ -10,32 +10,16 @@ namespace Palindrom
     {
         static void Main(string[] args)
         {
-
+            // https://www.youtube.com/watch?v=-Z5ZQ5s9Wno return
 
             Console.Write("Kelime giriniz : ");
             string girilenStr = Console.ReadLine();
 
-            char[] harfler = girilenStr.ToCharArray();
-            // int bölme işlem yapıldığında 5'i 2 ye bölersek sonuç 2 çıkar.
-            int döngüSayısı = harfler.Length / 2;
+            
 
-            bool polindromFlag = true;
+            bool PolindromKontrol = IsPolidrom(girilenStr);
 
-            // Döngü içinde flag kullanmak lazım
-            // Flag değişkenleri genelde bool olur
-            for (int i = 0; i < döngüSayısı; i++)
-            {
-                //Console.WriteLine(harfler[i]);
-
-                if (harfler[i] != harfler[harfler.Length -1 -i])
-                {
-                    //Console.WriteLine("Polindrom değil");
-                    polindromFlag = false;
-                    break;
-                }
-
-            }
-            if (polindromFlag == false)
+            if (PolindromKontrol == false)
             {
                 Console.WriteLine("Polindrom değildir");
             }
@@ -44,10 +28,41 @@ namespace Palindrom
                 Console.WriteLine("Polindromdur");
             }
 
-
-
-            // polindrom mu değil mi diye buraya yazılacak
+            Console.WriteLine("Program sonlanmıştır. Çıkmak için herhangi bir tuşa basınız...");
             Console.ReadKey();
+
         }
-    }
+
+        static bool IsPolidrom(string str)
+        {
+            // Döngü içinde flag kullanmak lazım
+            // Flag değişkenleri genelde bool olur
+            bool polindromFlag = true;
+
+            char[] harfler = str.ToCharArray();
+
+            // int bölme işlem yapıldığında 5'i 2 ye bölersek sonuç 2 çıkar.
+            int döngüSayısı = harfler.Length / 2;
+            for (int i = 0; i < döngüSayısı; i++)
+            {
+                //Console.WriteLine(harfler[i]);
+
+                if (harfler[i] != harfler[harfler.Length - 1 - i])
+                {
+                    //Console.WriteLine("Polindrom değil");
+                    polindromFlag = false;
+                    break;
+                }
+
+            }
+            //polindrom mu değil mi diye buraya yazılacak
+
+            return polindromFlag;
+
+        }
+
+            
+     }   
+
 }
+
---
+++ 35 PalindromDosya/PalindromDosya/PalindromDosya/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace PalindromDosya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Directory.CreateDirectory("C:\\Palindromm");
+            string DosyaYolu = @"C:\Palindromm\PalindromMetin.txt";
+
+            string okunanSatirlar = File.ReadAllText(DosyaYolu);
+            string[] kelimeler = okunanSatirlar.Split(' ', '#', '\n', '\r');
+
+           
+
+
+            for (int i = 0; i < kelimeler.Length; i++)
+            {
+                if (kelimeler[i].Length == 0)
+                {
+                    continue;
+                }
+
+                bool PolindromKontrol = IsPolidrom(kelimeler[i]);
+
+                if (PolindromKontrol == false)
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine(kelimeler[i]);
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+        static bool IsPolidrom(string str)
+        {
+            bool polindromFlag = true;
+
+            char[] harfler = str.ToCharArray();
+
+            int döngüSayısı = harfler.Length / 2;
+            for (int i = 0; i < döngüSayısı; i++)
+            {
+
+                if (harfler[i] != harfler[harfler.Length - 1 - i])
+                {
+                    polindromFlag = false;
+                    break;
+                }
+
+            }
+
+            return polindromFlag;
+
+        }
+    }
+
+}

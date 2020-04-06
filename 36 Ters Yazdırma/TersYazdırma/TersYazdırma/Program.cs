70d6b4342925e00b3a5a0c4d1dc140b60265f919
Sefa Kozan <sefakozan05@gmail.com>
2020-04-06 15:16:10 +0300

    Ters yazdirma eklendi.

--- 36 Ters Yazdırma/TersYazdırma/TersYazdırma/Program.cs
+++ 36 Ters Yazdırma/TersYazdırma/TersYazdırma/Program.cs
@@ -8,26 +8,80 @@ namespace TersYazdırma
 {
     class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// Main'in içinde bir satır kod olacak.
+        /// </summary>
+        static void Main()
         {
-            KareHesaplama();
+            StartTestYazdirma();
+        }
+        static void StartTestYazdirma()
+        {
+            int kare = KareHesapla();
+            Console.WriteLine("- Girdiğiniz sayının karesi : {0}", kare);
+            // sayinin karesini alıp stringe çevirip
+            string kareStr = Convert.ToString(kare);
+
+            string tersStr = ReverseString(kareStr);
+            Console.WriteLine("- Girdiğiniz sayının karesinin tersi : {0}", tersStr);
+
+            //Array.Reverse(sayilar);
+
+            // program bitti.
+            Console.WriteLine();
             Console.WriteLine("Program sonlandı. Çıkmak için herhangi bir tuşa basınız...");
             Console.ReadKey();
         }
-        static int KareHesaplama()
+
+        /// <summary>
+        /// kullanicidan sayi alir , sayinin karesini alır ve karesini dönderir.
+        /// </summary>
+        /// <returns>sayinin karesi</returns>
+        static int KareHesapla()
         {
             int kare, sayi; ;
             Console.Write("- Konsola bir sayı giriniz : ");
             string sayiStr = Console.ReadLine();
             sayi = Convert.ToInt32(sayiStr);
-
             // sayiyi kendisi ile çarpıp karesini buldum.
             kare = sayi * sayi;
-            Console.WriteLine("- Girdiğiniz sayının karesi : {0}", kare);
 
-            string[] harfler = new string[kare];
             return kare;
-
         }
+
+
+        /// <summary>
+        /// fonksiyon string'i parametre olarak alır ve ters çevirir.
+        /// </summary>
+        /// <param name="str"> ters cevrilecek string</param>
+        /// <returns> ters çevrilmiş string</returns>
+        static string ReverseString(string str)
+        {
+            string reversedStr = string.Empty;
+
+            char[] strArray = str.ToCharArray();
+            char[] reverseStrArray = new char[strArray.Length];
+
+            for (int i = 0; i < strArray.Length; i++)
+            {
+                int sonIndex = strArray.Length - i - 1;
+                reverseStrArray[i] = strArray[sonIndex];              
+            }
+
+            // 1. yol
+            // google: c# char array to string
+            reversedStr = new string(reverseStrArray);
+
+            //2. yol 
+
+            //StringBuilder sb = new StringBuilder();
+            //sb.Append(reverseStrArray);
+            //reversedStr = sb.ToString();
+
+            return reversedStr;
+        }
+
+
+
     }
 }
